b2d685e63687b2dea410c0bb95b57dcd620db76f
André Carvalho <afacarvalho@yahoo.com.br>
2019-09-03 11:36:47 -0300

    Config changes for deploy mongo data in production.

--- mongodb-businessapi.sh
+++ mongodb-businessapi.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-export_host="terrabrasilis2.dpi.inpe.br:30046"
-import_host="localhost:27018"
+# URL for homologation instance
+export_host="terrabrasilis2.dpi.inpe.br:30043"
+# URL for production instance
+import_host="terrabrasilis2.dpi.inpe.br:30046"
+
+# url for local instance
+#export_host="localhost:27018"
+# URL for homologation instance
+#import_host="terrabrasilis2.dpi.inpe.br:30043"
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Export
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 if [[ "$1" = "export" ]]; then
+    echo "Remove old files in bkp-collections..."
+    rm bkp-collections/*.json
     echo "exporting collections from production"
     ## download
     mongoexport --host $export_host --db businessapi --collection download --out bkp-collections/download.json
@@ -35,6 +44,13 @@ fi
 # Import
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++
 if [[ "$1" = "import" ]]; then
+    echo "Drop database \"businessapi\" of the $import_host HOST before import? Type yes to proceed..." ; read DROP
+    if [[ "$DROP" = "yes" ]]; then
+        mongo --host $import_host businessapi --eval "db.dropDatabase()"
+        
+    else
+        echo "Aborting drop...proceeding with import."
+    fi
     echo "importing collections to localhost"
     mongoimport --host $import_host --db businessapi --collection download --file bkp-collections/download.json
     mongoimport --host $import_host --db businessapi --collection tool --file bkp-collections/tool.json
