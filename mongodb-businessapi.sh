1ad152c261fb0bf078bc4440b9ab85a7e036a433
André Carvalho <afacarvalho@yahoo.com.br>
2019-12-09 17:48:34 -0200

    New backups of collections and upgrade version into pom.xml

--- mongodb-businessapi.sh
+++ mongodb-businessapi.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # URL for homologation instance
-#export_host="terrabrasilis2.dpi.inpe.br:30046"
+export_host="terrabrasilis2.dpi.inpe.br:30046"
 # URL for production instance
-#import_host="terrabrasilis2.dpi.inpe.br:30043"
+#import_host="terrabrasilis2.dpi.inpe.br:30046"
 
 # url for local instance
-export_host="localhost:27018"
-#import_host="localhost:27018"
-# URL for homologation instance
-import_host="terrabrasilis2.dpi.inpe.br:30043"
+#export_host="localhost:27018"
+import_host="localhost:27018"
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Export
