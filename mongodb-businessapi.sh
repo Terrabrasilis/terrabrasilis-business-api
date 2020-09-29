eccedcee1680725660f0eeeb909c1bea31bfc70c
André Carvalho <afacarvalho@yahoo.com.br>
2020-09-29 14:59:58 -0300

    Just changed the target port

--- mongodb-businessapi.sh
+++ mongodb-businessapi.sh
@@ -12,31 +12,32 @@ import_host="localhost:27017"
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Export
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++
+WORK_DIR="bkp-mongo-prod-without-biomes"
 
 if [[ "$1" = "export" ]]; then
-    echo "Remove old files in bkp-collections..."
-    rm bkp-collections/*.json
+    echo "Remove old files in $WORK_DIR..."
+    rm $WORK_DIR/*.json
     echo "exporting collections from production"
     ## download
-    mongoexport --host $export_host --db businessapi --collection download --out bkp-collections/download.json
+    mongoexport --host $export_host --db businessapi --collection download --out $WORK_DIR/download.json
 
     ## tool
-    mongoexport --host $export_host --db businessapi --collection tool --out bkp-collections/tool.json
+    mongoexport --host $export_host --db businessapi --collection tool --out $WORK_DIR/tool.json
 
     ## datasource
-    mongoexport --host $export_host --db businessapi --collection datasource --out bkp-collections/datasource.json
+    mongoexport --host $export_host --db businessapi --collection datasource --out $WORK_DIR/datasource.json
 
     ## subdomain
-    mongoexport --host $export_host --db businessapi --collection subdomain --out bkp-collections/subdomain.json
+    mongoexport --host $export_host --db businessapi --collection subdomain --out $WORK_DIR/subdomain.json
 
     ## layer
-    mongoexport --host $export_host --db businessapi --collection layer --out bkp-collections/layer.json
+    mongoexport --host $export_host --db businessapi --collection layer --out $WORK_DIR/layer.json
 
     ## vision
-    mongoexport --host $export_host --db businessapi --collection vision --out bkp-collections/vision.json
+    mongoexport --host $export_host --db businessapi --collection vision --out $WORK_DIR/vision.json
 
     ## vision_to_vision
-    mongoexport --host $export_host --db businessapi --collection vision_to_vision --out bkp-collections/vision_to_vision.json
+    mongoexport --host $export_host --db businessapi --collection vision_to_vision --out $WORK_DIR/vision_to_vision.json
 fi
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -51,13 +52,13 @@ if [[ "$1" = "import" ]]; then
         echo "Aborting drop...proceeding with import."
     fi
     echo "importing collections to localhost"
-    mongoimport --host $import_host --db businessapi --collection download --file bkp-collections/download.json
-    mongoimport --host $import_host --db businessapi --collection tool --file bkp-collections/tool.json
-    mongoimport --host $import_host --db businessapi --collection datasource --file bkp-collections/datasource.json
-    mongoimport --host $import_host --db businessapi --collection subdomain --file bkp-collections/subdomain.json
-    mongoimport --host $import_host --db businessapi --collection layer --file bkp-collections/layer.json
-    mongoimport --host $import_host --db businessapi --collection vision --file bkp-collections/vision.json
-    mongoimport --host $import_host --db businessapi --collection vision_to_vision --file bkp-collections/vision_to_vision.json
+    mongoimport --host $import_host --db businessapi --collection download --file $WORK_DIR/download.json
+    mongoimport --host $import_host --db businessapi --collection tool --file $WORK_DIR/tool.json
+    mongoimport --host $import_host --db businessapi --collection datasource --file $WORK_DIR/datasource.json
+    mongoimport --host $import_host --db businessapi --collection subdomain --file $WORK_DIR/subdomain.json
+    mongoimport --host $import_host --db businessapi --collection layer --file $WORK_DIR/layer.json
+    mongoimport --host $import_host --db businessapi --collection vision --file $WORK_DIR/vision.json
+    mongoimport --host $import_host --db businessapi --collection vision_to_vision --file $WORK_DIR/vision_to_vision.json
 fi
 
 if [[ "$1" = "" ]]; then
