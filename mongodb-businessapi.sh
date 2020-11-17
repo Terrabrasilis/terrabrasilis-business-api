d61c4dd56dc3ba1da5e87a8d8fd2d1bac42098a9
André Carvalho <afacarvalho@yahoo.com.br>
2020-11-17 20:53:23 -0300

    After publish data of biomes.

--- mongodb-businessapi.sh
+++ mongodb-businessapi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # URL for homologation instance
-#export_host="terrabrasilis2.dpi.inpe.br:30046"
+#export_host="terrabrasilis2.dpi.inpe.br:30043"
 # URL for production instance
-#import_host="terrabrasilis2.dpi.inpe.br:30043"
+#import_host="terrabrasilis2.dpi.inpe.br:30046"
 
 # url for local instance
 export_host="localhost:27017"
@@ -12,29 +12,32 @@ import_host="localhost:27017"
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Export
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++
-WORK_DIR="bkp-mongo-prod-without-biomes"
+# bkp-collections is data from server
+#WORK_DIR="bkp-collections"
+# bkp-collections-local is data from localhost
+#WORK_DIR="bkp-collections-local"
 
 if [[ "$1" = "export" ]]; then
     echo "Remove old files in $WORK_DIR..."
     rm $WORK_DIR/*.json
     echo "exporting collections from production"
     ## download
-    mongoexport --host $export_host --db businessapi --collection download --out $WORK_DIR/download.json
+    mongoexport --host $export_host --db businessapi --collection download --sort '{name:1,lang:1,created:1}' --out $WORK_DIR/download.json
 
     ## tool
-    mongoexport --host $export_host --db businessapi --collection tool --out $WORK_DIR/tool.json
+    mongoexport --host $export_host --db businessapi --collection tool --sort '{name:1,created:1}' --out $WORK_DIR/tool.json
 
     ## datasource
-    mongoexport --host $export_host --db businessapi --collection datasource --out $WORK_DIR/datasource.json
+    mongoexport --host $export_host --db businessapi --collection datasource --sort '{name:1,type:1,created:1}' --out $WORK_DIR/datasource.json
 
     ## subdomain
-    mongoexport --host $export_host --db businessapi --collection subdomain --out $WORK_DIR/subdomain.json
+    mongoexport --host $export_host --db businessapi --collection subdomain --sort '{name:1,created:1}' --out $WORK_DIR/subdomain.json
 
     ## layer
-    mongoexport --host $export_host --db businessapi --collection layer --out $WORK_DIR/layer.json
+    mongoexport --host $export_host --db businessapi --collection layer --sort '{name:1,workspace:1,created:1}' --out $WORK_DIR/layer.json
 
     ## vision
-    mongoexport --host $export_host --db businessapi --collection vision --out $WORK_DIR/vision.json
+    mongoexport --host $export_host --db businessapi --collection vision --sort '{name:1,created:1}' --out $WORK_DIR/vision.json
 
     ## vision_to_vision
     mongoexport --host $export_host --db businessapi --collection vision_to_vision --out $WORK_DIR/vision_to_vision.json
